6555be956a1ced6f50a3744428d6061ef374fac6
bind <douglasjbinder@gmail.com>
2016-07-10 19:22:42 -0400

    small changds and style

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM  from 'react-dom';
 import {Editor, EditorState} from 'draft-js';
 
+if (typeof window !== 'undefined') {
+    window.React = React;
+}
+
 class Entry extends React.Component {
 	constructor(props){
 		super(props);
 	    this.state = {editorState: EditorState.createEmpty()};
     	this.onChange = (editorState) => this.setState({editorState});
+    	this.focus = () => this.refs.editor.focus()
 	}
 	render(){
   		    const {editorState} = this.state;
-    		return <Editor editorState={editorState} onChange={this.onChange} />;
+    		return <div onClick={this.focus}> <Editor editorState={editorState} onChange={this.onChange} ref="editor" /></div>;
 	}
 }
 
